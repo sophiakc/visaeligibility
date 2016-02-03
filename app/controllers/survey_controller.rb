6684cbf645764a1693f9d5cfd65adc866415aa71
sophiakc <sophiakc@gmail.com>
2016-02-03 19:20:31 +0100

    création de la vue et mise en place du radio button #1 avec Thomas

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -2,5 +2,17 @@ class SurveyController < ApplicationController
   def question
     @asked_question = Question.find_by(id: params[:id])
     @answers_possible = @asked_question.possible_answers
+    @user_answer = UserAnswer.new
+  end
+
+  def answer
+    UserAnswer.create(user_answer_params) #sauvegarde la réponse, affiche une page avec la question suivante, renvoit l'utilisateur sur la prochaine question
+    #trouver la following question
+  end
+
+  private def user_answer_params
+    params.require(:user_answer).permit(:possible_answer_id, :user_id)
+    # ds la clé user_answer je permets certains strong parameters
+    # regarder la doc de "strong parameters" et remplir permit(...)
   end
 end
