9b95b01410539f3fbea898228a3d3e199874ba05
sophiakc <sophiakc@gmail.com>
2016-03-11 16:51:39 +0100

    front not yet solved but in progress

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'survey/question/:id' => 'survey#question', as: 'question'
   post 'survey/answer'     => 'survey#answer'
   get 'result/:id' => 'result#result', as: 'result'
+
+  get "/" => 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
